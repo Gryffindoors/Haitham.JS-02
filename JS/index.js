b07f27bdb2d13fbd434e9bb99f067258ec60afe7
Gryffindoors <goldengryffin010@gmail.com>
2024-11-26 22:52:02 +0200

    code finished

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+var repeatX = Number(prompt('Please enter requested repetitions'));
+var starPrint="";
+var i=0
+var j=0
+for(i = 0; i < repeatX; i++) {
+    for (j = 0; j < i; j++){
+        starPrint += '<i class="fa-solid fa-star m-2 text-dark-emphasis"></i>';
+    }
+    starPrint +='<br>'
+}
+document.getElementById('jsWritten').innerHTML = starPrint;
